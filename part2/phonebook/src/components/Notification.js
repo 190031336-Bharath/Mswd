cc1569af31f46aff07d733f3a84af477d6a348aa
Bharath-popuri <84761751+Bharath-popuri@users.noreply.github.com>
2021-06-01 23:30:21 +0530

    Update Notification.js

--- part2/phonebook/src/components/Notification.js
+++ part2/phonebook/src/components/Notification.js
@@ -1,24 +1,28 @@
 import React from 'react'
 
-const Notification = ({ message, messageState }) => {
-    if (message == null) {
+const Notification = ({ message, messageState }) =>
+ {
+    if (message == null) 
+    {
         return null
-    }
-    if (messageState === 'success') {
+    };
+    if (messageState === 'success') 
+    {
         return (
             <div className="success">
                 {message}
             </div>
-        )
-    } else if (messageState === 'error') {
+        );
+    } else if (messageState === 'error') 
+    {
         return (
             <div className="error">
                 {message}
             </div>
-        )
-    }
+        );
+    };
 
 
-}
+};
 
-export default Notification
+export default Notification;
